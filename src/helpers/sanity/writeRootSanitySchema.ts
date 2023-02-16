b83a9e9ae8b1d190e48cf3fc38c9961e3a8a5d16
Rune Botten <rbotten@gmail.com>
2023-02-15 21:38:10 -0800

    feat: Write V3 schemas and output document and objects in subfolders

--- src/helpers/sanity/writeRootSanitySchema.ts
+++ src/helpers/sanity/writeRootSanitySchema.ts
@@ -10,25 +10,21 @@ export async function writeRootSanitySchema(
   const schemasDir = path.join(dir ?? process.cwd(), 'schemas')
   await fs.ensureDir(schemasDir)
 
-  const importStatements = schemas.map(schema => (
-    `import ${Case.camel(schema.name)} from './${schema.name}.js'`
-  )).join('\n')
+  const importStatements = schemas.map(schema => {
+    return `import {${Case.camel(schema.name)}Type} from './${schema.type === 'document' ? 'documents' : 'objects'}/${schema.name}.js'`
+  }).join('\n')
   const typesConcatList = schemas.map(schema => (
-    `    ${Case.camel(schema.name)},`
+    `  ${Case.camel(schema.name)}Type,`
   )).join('\n')
 
   await fs.writeFile(
     path.join(schemasDir, 'schema.js'),
-    `
-import createSchema from 'part:@sanity/base/schema-creator'
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+`
 ${importStatements}
 
-export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([
+export const schemas = [
 ${typesConcatList}
-  ])
-})`,
+]
+`,
   )
 }
