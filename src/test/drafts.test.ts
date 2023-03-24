8bcb815067aefe6f6363a18f1b2655ad152319d0
Rune Botten <rbotten@gmail.com>
2023-03-24 13:48:12 -0700

    chore: add test for draft linking to draft

--- src/test/drafts.test.ts
+++ src/test/drafts.test.ts
@@ -1,50 +1,53 @@
-import {describe, expect, test} from 'vitest'
+import {SanityDocument} from '@sanity/client'
+import {beforeEach, describe, expect, test} from 'vitest'
 
 import {contentfulToDataset} from '../helpers/contentfulToDataset'
 import {parse} from './helpers'
 
+declare module 'vitest' {
+  export interface TestContext {
+    docs: SanityDocument[]
+  }
+}
+
+beforeEach(async (context) => {
+  const {default: drafts} = await import('./fixtures/drafts.json')
+  const {default: published} = await import('./fixtures/drafts.published.json')
+  const options = {
+    intlMode: 'single',
+    weakRefs: false,
+    intlIdStructure: 'delimiter',
+    keepMarkdown: false,
+    locale: undefined,
+  } as const
+
+  const ndjson = await contentfulToDataset(
+    {
+      drafts: drafts as any,
+      published: published as any,
+    },
+    options,
+  )
+  context.docs = ndjson.split('\n').map(parse)
+})
+
 describe('Drafts', async () => {
-  test('adds drafts from additional export', async () => {
-    const {default: drafts} = await import('./fixtures/drafts.json')
-    const {default: published} = await import('./fixtures/drafts.published.json')
-    const options = {
-      intlMode: 'single',
-      weakRefs: false,
-      intlIdStructure: 'delimiter',
-      keepMarkdown: false,
-      locale: undefined,
-    } as const
+  test('Draft doc with a draft reference', async ({docs}) => {
+    const post = docs.find((doc) => doc._id === 'drafts.4RIfygf1stLd0J1gycYXlu')
+    expect(post).toBeDefined()
+    const person = docs.find((doc) => doc._id === 'drafts.6PRft3TsWQAf2Fkeb6ZKhb')
+    expect(person).toBeDefined()
 
-    const ndjson = await contentfulToDataset(
-      {
-        drafts: drafts as any,
-        published: published as any,
+    expect(post?.author).toEqual({
+      _ref: person?._id,
+      _type: 'reference',
+      _weak: true,
+      _strengthenOnPublish: {
+        type: 'person',
+        template: {
+          id: 'person',
+        },
       },
-      options,
-    )
-    const docs = ndjson.split('\n').map(parse)
-
-    // Check for duplicates in docs array based on _id
-    const ids = docs.map((doc) => doc._id)
-    expect(ids).toHaveLength(new Set(ids).size)
-
-    // Exists only as drafts
-    const draftIds = [`6tQpUjsZ4RyaSzxqNd0Id3`, `9lDgwws6jtn8ow1CkWDmA`]
-    for (const id of draftIds) {
-      expect(docs.find((doc) => doc._id === `drafts.${id}`)).toBeDefined()
-      // Should not be a published document
-      expect(docs.filter((doc) => doc._id === id)).toHaveLength(0)
-    }
-
-    // Published and draft
-    expect(docs.filter((doc) => doc._id === '5NCD8ztIrVWo7MMBf9f81D')).toHaveLength(1)
-    expect(docs.find((doc) => doc._id === 'drafts.5NCD8ztIrVWo7MMBf9f81D')).toBeDefined()
-
-    // Published doc with a broken draft ref
-    const doc = docs.find((document) => document._id === '6T4f1JKI3KbeqElChsqUZs')
-    expect(doc).toBeDefined()
-    expect(doc.author).toBeNull()
+    })
   })
-
-  test('Draft doc with a draft reference')
 })
