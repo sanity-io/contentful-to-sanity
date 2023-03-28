c02a91a74190d0c0ac36fe92f54c503dc4407a78
Rune Botten <rbotten@gmail.com>
2023-03-28 14:24:38 -0700

    build: fix failing test because sharp initialized in worker threads

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+// https://github.com/vitest-dev/vitest/issues/740
+// We need to import sharp here to avoid the test runner crashing.
+// Likely because it is trying to initialize sharp in worker threads,
+// and this is the only location where we are not in a worker thread.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import sharp from 'sharp'
 import {defineConfig} from 'vitest/config'
 import GithubActionsReporter from 'vitest-github-actions-reporter'
 
