6e1be4e76a725ddba1ebe82220b26c822f450460
Rune Botten <rbotten@gmail.com>
2023-04-04 16:00:48 -0700

    test: add test for dataset action on PTE block embeds

--- src/test/pteReferences.test.ts
+++ src/test/pteReferences.test.ts
@@ -47,46 +47,69 @@ beforeEach(async (context) => {
 })
 
 describe('PTE block-level references', async () => {
-  test('handles unrestricted links', async ({schemas}) => {
-    const post = schemas.find((schema) => schema.name === 'post')
-    expect(post).toBeDefined()
-    const pteField = post?.fields.find((field) => field.name === 'body') as ArraySanityFieldSchema
-    expect(pteField).toBeDefined()
+  describe('schema', async () => {
+    test('handles unrestricted links', async ({schemas}) => {
+      const post = schemas.find((schema) => schema.name === 'post')
+      expect(post).toBeDefined()
+      const pteField = post?.fields.find((field) => field.name === 'body') as ArraySanityFieldSchema
+      expect(pteField).toBeDefined()
 
-    expect(pteField.of).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          type: 'reference',
-          to: expect.arrayContaining([
-            expect.objectContaining({type: 'author'}),
-            expect.objectContaining({type: 'post'}),
-          ]),
-        }),
-      ]),
-    )
+      expect(pteField.of).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            type: 'reference',
+            to: expect.arrayContaining([
+              expect.objectContaining({type: 'author'}),
+              expect.objectContaining({type: 'post'}),
+            ]),
+          }),
+        ]),
+      )
+    })
+
+    test('handles links limited to a type', async ({schemas}) => {
+      const post = schemas.find((schema) => schema.name === 'post')
+      expect(post).toBeDefined()
+      const pteField = post?.fields.find(
+        (field) => field.name === 'intro',
+      ) as ArraySanityFieldSchema
+      expect(pteField).toBeDefined()
+
+      expect(pteField.of).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            type: 'block',
+          }),
+        ]),
+      )
+
+      expect(pteField.of).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            type: 'reference',
+            to: [{type: 'author'}],
+          }),
+        ]),
+      )
+    })
   })
 
-  test('handles links limited to a type', async ({schemas}) => {
-    const post = schemas.find((schema) => schema.name === 'post')
-    expect(post).toBeDefined()
-    const pteField = post?.fields.find((field) => field.name === 'intro') as ArraySanityFieldSchema
-    expect(pteField).toBeDefined()
+  describe('dataset', async () => {
+    test('creates reference from embedded entries', async ({dataset}) => {
+      const post = dataset.find(
+        (doc) => doc._type === 'post' && doc._id === 'drafts.1B48wTtOpUhuuEoNkTDji2',
+      )
+      expect(post).toBeDefined()
+      if (!post) return
 
-    expect(pteField.of).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          type: 'block',
-        }),
-      ]),
-    )
+      // Should link to a specific author
+      expect(post.body[3]).toHaveProperty('_type', 'reference')
+      expect(post.body[3]).toHaveProperty('_ref', '5JpJ63LfU5itcEFXvgpA4Z')
 
-    expect(pteField.of).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          type: 'reference',
-          to: [{type: 'author'}],
-        }),
-      ]),
-    )
+      // Should link to a specific draft author
+      expect(post.body[5]).toHaveProperty('_type', 'reference')
+      expect(post.body[5]).toHaveProperty('_strengthenOnPublish')
+      expect(post.body[5]).toHaveProperty('_ref', 'drafts.wLhL9uUiZUHRC28p9Ix8E')
+    })
   })
 })
