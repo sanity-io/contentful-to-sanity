95996b02d3d59d5a75ff192bb38791f6ef93b5b3
Rune Botten <rbotten@gmail.com>
2023-04-03 22:45:15 -0700

    fix: add test and fix handling booleans from export (#70)
    Correctly handles boolean values from export

--- src/utils/contentfulEntryToSanityObject.ts
+++ src/utils/contentfulEntryToSanityObject.ts
@@ -60,7 +60,7 @@ export function contentfulEntryToSanityObject(
     const widgetId = control?.widgetId
 
     const value = values[locale]
-    const canCopyValueAsIs = typeof value === 'string' || typeof value === 'number'
+    const canCopyValueAsIs = typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean'
     if (canCopyValueAsIs) {
       if (widgetId === 'slugEditor') {
         doc[key] = {current: value}
